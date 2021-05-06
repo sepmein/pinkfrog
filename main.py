bb25bf42774c21e505677c783fa75ae76eb715ec
sepmein <sepmein@gmail.com>
2021-05-06 17:24:26 +0200

    updated the pf framework and exmaples

--- main.py
+++ main.py
@@ -6,52 +6,139 @@
 from typing import Any, Callable
 
 import tensorflow as tf
+from tensorflow.python.ops.array_ops import stack
 import tensorflow_probability as tfp
+import numpy as np
 
-class Individual():
-    """
-    Individual is the basic build block of the whole dynamic system.
-    Individual could have several **states**, each state is essentially a number, for example: a human has a state called age, and it is a number from 0 to some number.
-    Could a state be represented by a group of numbers? For now, I don't find the case.
-    Here I want to propose an example of dynamic and stochastic birth system to simulate the birth and death of population
-    """
-    def __init__(self) -> None:
-        self.states = []
+
+class TargetGroup(object):
+    """Docstring for TargetGroup. """
+
+    def __init__(self, name: str, n: int) -> None:
+        """TODO: to be defined.
+
+        :Docstring for TargetGroup.: TODO
+
+        """
+        self.name = name
+        self.n = n
+        self.tensor = None
+        self.state = {}
+        self.state_len = len(self.state)
+
+    def add_state(self, state: Any) -> None:
+        initiated = state.generator(self.n)
+        print(initiated)
+        initiated = tf.transpose(initiated)
+        print(initiated)
+        self.state[state.name] = {
+            "name": state.name,
+            "index": self.state_len + 1,
+            "transistor": state.transistor
+        }
+
+        if self.tensor is not None:
+            tf.stack([self.tensor, initiated], axis=1)
+        else:
+            self.tensor = initiated
+
+    def remove_state(self, state: Any) -> None:
         pass
 
-    def set(self, state:Any) -> Any:
-        self.states.append(state)
-        return self
+    def next(self) -> None:
+        for key, state in self.state.items():
+            tensor_slice_index = state['index']
+            transistor = state['transistor']
+            self.tensor = transistor(self.tensor, tensor_slice_index)
+
 
 class State():
-    def __init__(self, name: str, value: Any, next: Callable ) -> None:
+    def __init__(self,
+                 name: str,
+                 transistor: Callable,
+                 generator: Callable) -> None:
         self.name = name
-        self.value = value
-        self.next = next
-        return self
+        self.transistor = transistor
+        self.generator = generator
 
-class Group():
-    def __init__(self, individual: Individual, number: int) -> None:
-        if (type(individual) is not Individual):
-            raise Exception()
-        pass
 
-class Disease():
-  def __init__(self, name):
-        # disease name
-        self.name = name 
+class Transistor():
+    def __init__(self) -> None:
+        super().__init__()
 
-class Disease_COVID(Disease):
+    @staticmethod
+    def add(number: int) -> Callable:
+        def returned_add(tensor, tensor_slice_index):
+            data_width = tensor.shape
+            to_add_tensor = np.zeros(data_width)
+            to_add_tensor[tensor_slice_index] = number
+            result_tensor = tf.math.add(tensor, to_add_tensor)
+            print(result_tensor)
+            return result_tensor
+
+        return returned_add
+
+
+class Generator():
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Environment():
+    """Docstring for Environment. """
+
     def __init__(self):
-        # inherit from disease
-        super(Disease, self).__init__()
+        """TODO: to be defined.
 
-class People():
-    def __init__(self):
-        return
+        :Docstring for Environment.: TODO
 
-Spencer = Individual()
-def age_next(age):
-    return age + 1
+        """
+        self.time = 0
+        self.target_group = []
+        Environment.__init__(self)
 
-age = State('age', 33, age_next)
+    def add_target_group(self, target_group: Any) -> None:
+        self.target_group.append(target_group)
+
+    def next(self, steps: int):
+        for _ in range(steps):
+            self.time = self.time + 1
+            for i in self.target_group:
+                self.target_group[i].next()
+
+# class Individual():
+#    """
+#    Individual is the basic build block of the whole dynamic system.
+#    Individual could have several **states**, each state is essentially a number, for example: a human has a state called age, and it is a number from 0 to some number.
+#    Could a state be represented by a group of numbers? For now, I don't find the case.
+#    Here I want to propose an example of dynamic and stochastic birth system to simulate the birth and death of population
+#    """
+#    def __init__(self) -> None:
+#        self.states = []
+#        pass
+#
+#    def set(self, state:Any) -> Any:
+#        self.states.append(state)
+#        return self
+#
+#
+# class Group():
+#    def __init__(self, individual: Individual, number: int) -> None:
+#        if (type(individual) is not Individual):
+#            raise Exception()
+#        pass
+#
+# class Disease():
+#  def __init__(self, name):
+#        # disease name
+#        self.name = name 
+#
+# class Disease_COVID(Disease):
+#    def __init__(self):
+#        # inherit from disease
+#        super(Disease, self).__init__()
+#
+# class People():
+#    def __init__(self):
+#        return
+#
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
-# Pink Fog - General Interface for Infectious Disease Modelling
+# PinkFrog
+Agent Based Model + Probabilistic Programming + Deep Learning
+
+Can we bind these powerful tools together?
+
+---
+
+This project started with a general purpose to understand how to develop an infectious disease model to help reasoning disease.
+
+In the study, I found ABM with probabilistic programming was a powerful tool to describe the trending of infectious diseases.
+
+But sophisticated models are way too complicated to develop. It often took several years to be useful.
+
+Based on Tensorflow and/or Pytorch
 
 
