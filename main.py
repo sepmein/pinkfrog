8b6f05bc2d0e6c19814ef895a1893b661813221e
sepmein <sepmein@gmail.com>
2021-05-22 15:34:48 +0200

    added pytest

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # TODO: create a human class
 # TODO: create an attribute class
 
-from typing import Any, Callable
+from typing import Any, Callable, List
 
 import tensorflow as tf
 from tensorflow.python.ops.array_ops import stack
@@ -12,7 +12,7 @@ import numpy as np
 
 
 class TargetGroup(object):
-    """Docstring for TargetGroup. """
+    """Docstring for TargetGroup."""
 
     def __init__(self, name: str, n: int) -> None:
         """TODO: to be defined.
@@ -28,15 +28,18 @@ class TargetGroup(object):
     def add_state(self, state: Any) -> None:
         for key, value in self.state.items():
             if state.name == key:
-                raise NameError('pf.add_state, state name:', state.name, ' has already in the group.')
+                raise NameError(
+                    "pf.add_state, state name:",
+                    state.name,
+                    " has already in the group.",
+                )
         initiated = state.generator(self.n)
         self.state[state.name] = {
-            "name": state.name,
             "index": len(self.state),
-            "transistor": state.transistor
+            "transistor": state.transistor,
         }
         if self.tensor is not None:
-            self.tensor = tf.stack([self.tensor, initiated], axis=1)
+            self.tensor = tf.stack([self.tensor, initiated])
         else:
             self.tensor = initiated
 
@@ -45,49 +48,160 @@ class TargetGroup(object):
 
     def next(self) -> None:
         for key, state in self.state.items():
-            tensor_slice_index = state['index']
-            transistor = state['transistor']
+            tensor_slice_index = state["index"]
+            transistor = state["transistor"]
             self.tensor = transistor(self.tensor, tensor_slice_index)
 
+    def _get_index_by_name_(self, name_list: List):
+        # get index
+        index = []
+        for name in name_list:
+            if name in self.state:
+                index.push(self.state[name].index)
+            else:
+                raise Exception("Name not in state list")
+        # get tensor by index
+        return index
 
-class State():
-    def __init__(self,
-                 name: str,
-                 transistor: Callable,
-                 generator: Callable) -> None:
+
+class State:
+    def __init__(
+        self,
+        name: str,
+        transistor: Callable,
+        generator: Callable,
+        related_state: List = None,
+    ) -> None:
         self.name = name
+        self.related_state = related_state
         self.transistor = transistor
         self.generator = generator
 
 
-class Transistor():
+class Transistor:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
+
+    @staticmethod
+    def input(*names):
+        return names
+
+    @staticmethod
+    def layer(fn) -> Callable:
+        def layer_fn(input):
+            return fn(input)
+
+        return layer_fn
+
+    @staticmethod
+    def _get_slice(tensor, tensor_slice_index):
+        dimension = len(tensor.shape)
+        if dimension == 1:
+            return tensor
+        elif dimension == 2:
+            return tensor[tensor_slice_index, :]
+
+    @staticmethod
+    def _update_tensor(tensor, tensor_slice_index, to_update_tensor):
+        dimension = len(tensor.shape)
+        if dimension == 1:
+            # 1d updates
+            indices = np.arange(tensor.shape[0])
+            indices = indices.reshape([-1, 1])
+            return tf.tensor_scatter_nd_update(tensor, indices, to_update_tensor)
+        elif dimension == 2:
+            # row update
+            indices = tf.constant([[tensor_slice_index]])
+            to_update_tensor = tf.reshape(to_update_tensor, [1, -1])
+            return tf.tensor_scatter_nd_update(tensor, indices, to_update_tensor)
+
+    @staticmethod
+    def add(number: int) -> Callable:
+        def returned_add(tensor, tensor_slice_index):
+            target_tensor = Transistor._get_slice(tensor, tensor_slice_index)
+            to_update_tensor = target_tensor + number
+            return Transistor._update_tensor(
+                tensor, tensor_slice_index, to_update_tensor
+            )
+
+        #            if dimension == 1:
+        #                return tensor + number
+        #            elif dimension == 2:
+        #                data_width = tensor.shape[1]
+        #                to_add_tensor = np.zeros(data_width)
+        #                to_add_tensor[tensor_slice_index] = number
+        #                result_tensor = tf.math.add(tensor, to_add_tensor)
+        #                return result_tensor
+        #
+        return returned_add
+
+    @staticmethod
+    def multiply(number: int) -> Callable:
+        def returned_multiply(tensor, tensor_slice_index):
+            target_tensor = Transistor._get_slice(tensor, tensor_slice_index)
+            to_update_tensor = target_tensor * number
+            return Transistor._update_tensor(
+                tensor, tensor_slice_index, to_update_tensor
+            )
+
+        return returned_multiply
+
+    @staticmethod
+    def bernoulli_flip(logits=None, probs=None) -> Callable:
+        def return_bernoulli_flip(tensor, tensor_slice_index):
+            # get target tensor, cast into int32
+            target_tensor = Transistor._get_slice(tensor, tensor_slice_index)
+            target_tensor = tf.cast(target_tensor, dtype=tf.int32)
+            # generate sample
+            # using bernoulli distribution
+            tensor_size = tensor.shape
+            tensor_columns = tensor_size[1]
+            sample = tfp.distributions.Bernoulli(
+                logits=logits, probs=probs, dtype=tf.int32
+            ).sample(tensor_columns)
+            # cal bitwise xor
+            to_update_tensor = tf.bitwise.bitwise_xor(target_tensor, sample)
+            to_update_tensor = tf.cast(to_update_tensor, tf.float32)
+            # update original tensor
+            return Transistor._update_tensor(
+                tensor, tensor_slice_index, to_update_tensor
+            )
+
+        return return_bernoulli_flip
+
+
+class Suspectible_Infection_Proprotion_Calculator(Transistor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return super().__call__(*args, **kwds)
+
+class Neural_Network_Probability_Distribution(Transistor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return super().__call__(*args, **kwds)
+
+
+class Generator:
     def __init__(self) -> None:
         super().__init__()
 
     @staticmethod
-    def add(number: int) -> Callable:
+    def ones():
+        def generate_ones(n):
+            return tf.ones(n, dtype=tf.float32)
 
-        def returned_add(tensor, tensor_slice_index):
-            dimension = len(tensor.shape)
-            if dimension == 1:
-                return tensor + number
-            elif dimension == 2:
-                data_width = tensor.shape[1]
-                to_add_tensor = np.zeros(data_width)
-                to_add_tensor[tensor_slice_index] = number
-                result_tensor = tf.math.add(tensor, to_add_tensor)
-                return result_tensor
-
-        return returned_add
+        return generate_ones
 
 
-class Generator():
-    def __init__(self) -> None:
-        super().__init__()
-
-
-class Environment():
-    """Docstring for Environment. """
+class Environment:
+    """Docstring for Environment."""
 
     def __init__(self):
         """TODO: to be defined.
@@ -107,6 +221,7 @@ class Environment():
             self.time = self.time + 1
             for i in self.target_group:
                 self.target_group[i].next()
+
 
 # class Individual():
 #    """
@@ -133,7 +248,7 @@ class Environment():
 # class Disease():
 #  def __init__(self, name):
 #        # disease name
-#        self.name = name 
+#        self.name = name
 #
 # class Disease_COVID(Disease):
 #    def __init__(self):
--- main_test.py
+++ main_test.py
@@ -1,6 +1,10 @@
 import main as pf
+import tensorflow as tf
 import tensorflow_probability as tfp
 import pytest
+import numpy as np
+import main.Transistor as pft
+
 
 def test_import():
     Uganda = pf.TargetGroup(name='Uganda', n=1000)
@@ -18,6 +22,7 @@ def test_add_state():
     Uganda.next()
     assert (len(Uganda.state) == 1)
 
+
 def test_add_two_state():
     Uganda = pf.TargetGroup(name='Uganda', n=100)
     age = pf.State(name='age',
@@ -31,8 +36,9 @@ def test_add_two_state():
     Uganda.add_state(age)
     Uganda.add_state(dummy_state)
     assert (len(Uganda.state) == 2)
-    assert (Uganda.tensor.shape == [100, 2])
+    assert (Uganda.tensor.shape == [2, 100])
     Uganda.next()
+
 
 def test_add_two_same_name_state():
     Uganda = pf.TargetGroup(name='Uganda', n=100)
@@ -47,3 +53,58 @@ def test_add_two_same_name_state():
     with pytest.raises(NameError):
         Uganda.add_state(age)
         Uganda.add_state(dummy_state)
+
+
+def test_si_model():
+    SI = pf.TargetGroup(name='si', n=10)
+    age = pf.State(name='age',
+                   transistor=pf.Transistor.add(number=1),
+                   generator=tfp.distributions.NegativeBinomial(total_count=100, probs=0.4).sample)
+
+    def calculate_si_percentage(tensor_list):
+        tensor = tensor_list
+        S = tf.reduce_sum(tensor).numpy()
+        N = len(tensor.shape)
+        I = N - S
+        return I * (S / N)
+
+    input = pft.input('disease_susceptible')
+    cal_si_p = pft.layer(calculate_si_percentage)(input)
+    transistor = pft.bernoulli_flip()(cal_si_p)
+
+    # transitor
+    # input(what transistor should know about the tensor): 'disease_susceptible'
+    # fn: calculate_si_percentage, bernoulli_flip
+    disease_suspectible = pf.State(name='disease_susceptible',
+                                   transistor=transistor,
+                                   generator=pf.Generator.ones())
+    SI.add_state(disease_suspectible)
+    comparison = SI.tensor.numpy() == np.ones(10)
+    assert (comparison.all())
+
+    # add another state
+    SI.add_state(age)
+    print(SI.tensor)
+    SI.next()
+    print(SI.tensor)
+    SI.next()
+    print(SI.tensor)
+    SI.next()
+    print(SI.tensor)
+    SI.next()
+    print(SI.tensor)
+
+def test_add_multiple_correlated_state():
+    dummy = pf.TargetGroup(name='dummy', n = 100)
+    def transistor_fn():
+        def return_fn(tensor, self_state_index, related_tensor_index_list):
+
+    a = pf.State(name='a',
+                 related_state=['b'],
+                 generator=pf.Generator.ones,
+                 transistor=)
+
+    b = pf.State(name='b',
+                 related_state=['a'],
+                 generator=pf.Generator.ones,
+                 transistor= )
